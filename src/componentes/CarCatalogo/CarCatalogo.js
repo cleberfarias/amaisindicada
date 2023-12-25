d8efe29204f6e6baea85bbb23bef1d9f945e524d
cleberfarias <cleber_afd@hotmail.com>
2023-12-25 19:39:10 -0300

    atuliazação catalogo

--- src/componentes/CarCatalogo/CarCatalogo.js
+++ src/componentes/CarCatalogo/CarCatalogo.js
@@ -1,92 +1,130 @@
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import IconesHeader from '../IconesHeader/IconesHeader';
 import Prata from '../../imagens/prata.svg';
 import Ouro from '../../imagens/ouro.svg';
-import IconesHeader from '../IconesHeader/IconesHeader';
-
+import Maracuja from '../../imagens/maracuja.svg';
 
 const CarContainer = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-around;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
 `;
 
 const Card = styled.div`
-    align-items: center;
-    background-color: #FFF;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    border-radius: 10px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    margin: 20px;
-    max-width: 300px;
-    padding: 5px 5px 5px;
+  align-items: center;
+  background-color: #FFF;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin: 20px;
+  max-width: 300px;
+  padding: 5px 5px 5px;
 `;
 
 const Botao = styled.button`
-    background-color: #EB9B00;
-    color: #FFF;
-    padding: 5px 0;
-    font-size: 14px;
-    border-radius: 15px;
-    font-weight: 900;
-    cursor: pointer;
-    text-decoration: none;
-    width: 100%;
-    margin-top: 5px;
+  background-color: #EB9B00;
+  color: #FFF;
+  padding: 5px 0;
+  font-size: 14px;
+  border-radius: 15px;
+  font-weight: 900;
+  cursor: pointer;
+  text-decoration: none;
+  width: 100%;
+  margin-top: 5px;
 `;
 
-
-
 const ImgSabor = styled.img`
-    max-height: 300px;
-    margin-bottom: 0;
-    padding: auto 0 10px ;
+  max-height: 300px;
+  margin-bottom: 0;
+  padding: auto 0 10px ;
 `;
 
 const Valor = styled.p`
-    text-align: center;
-    font-size: 18px;
-    font-weight: bold;
-    margin-top: 1px;
+  text-align: center;
+  font-size: 18px;
+  font-weight: bold;
+  margin-top: 1px;
 `;
 
 const InconesCatalago = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-    margin-top: 1px;
-    margin-bottom: 1px;  /* Ajustei o margin-bottom para reduzir a distância */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  margin-top: 1px;
+  margin-bottom: 1px;
 `;
+
 const TextoCatalogo = styled.div`
-color: #EB9B00;
-font-size: 16px;
-font-weight: bold;
-margin: 10px 0;
-text-align: center;
-`
+  color: #EB9B00;
+  font-size: 16px;
+  font-weight: bold;
+  margin: 10px 0;
+  text-align: center;
+`;
+
+function ProductCard({ title, image, price, onBuyNowClick }) {
+  const handleBuyNowClick = () => {
+    onBuyNowClick({ title, image, price });
+  };
+
+  return (
+    <Card>
+      <div>
+        <TextoCatalogo>{title}</TextoCatalogo>
+        <ImgSabor src={image} alt={title} />
+        <Valor>R$ {price.toFixed(2)}</Valor>
+        <InconesCatalago><IconesHeader /></InconesCatalago>
+        <Botao onClick={handleBuyNowClick}>Comprar agora</Botao>
+      </div>
+    </Card>
+  );
+}
 
 function CardCatalogo() {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+  };
+
+  const products = [
+    {
+      id: 1,
+      title: 'Cachaça Prata',
+      image: Prata,
+      price: 25.00,
+    },
+    {
+      id: 2,
+      title: 'Cachaça Ouro',
+      image: Ouro,
+      price: 25.00,
+    },
+    {
+      id: 3,
+      title: 'Licor Maracuja',
+      image: Maracuja,
+      price: 25.00,
+    },
+    // Adicione mais produtos conforme necessário
+  ];
+
   return (
     <CarContainer>
-      <Card>
-        <div>
-          <TextoCatalogo>Cachaça Prata</TextoCatalogo>
-          <ImgSabor src={Prata} />
-          <Valor>R$ 25,00</Valor>
-          <InconesCatalago><IconesHeader /></InconesCatalago>
-          
-          
-        </div>
-      </Card>
-      <Card>
-        <div>
-          <ImgSabor src={Ouro} />
-          <Valor>R$ 25,00</Valor>
-          <Botao href="#">Saiba mais</Botao>
-        </div>
-      </Card>
+      {products.map(product => (
+        <ProductCard
+          key={product.id}
+          title={product.title}
+          image={product.image}
+          price={product.price}
+          onBuyNowClick={addToCart}
+        />
+      ))}
     </CarContainer>
   );
 }
--- src/rotas/sacola.js
+++ src/rotas/sacola.js
@@ -1,17 +1,47 @@
+import React, { useState } from 'react';
 import styled from 'styled-components';
-
 
 const AppContainer = styled.div`
   width: 100vw;
   height: 100vh;
   background-image: linear-gradient(90deg,#002f52 35%,#326589 165%);
 `
+
+const Button = styled.button`
+  background-color: #EB9B00;
+  color: #FFF;
+  padding: 5px 0;
+  font-size: 14px;
+  border-radius: 15px;
+  font-weight: 900;
+  cursor: pointer;
+  text-decoration: none;
+  width: 100%;
+  margin-top: 5px;
+`;
+
 function Sacola() {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (item) => {
+    setCart([...cart, item]);
+  };
+
   return (
     <AppContainer>
-      
+      <h1>Sacola de Compras</h1>
+      <ul>
+        {cart.map((item, index) => (
+          <li key={index}>{item.title} - R$ {item.price.toFixed(2)}</li>
+        ))}
+      </ul>
+      {/* Renderize seu componente de catálogo aqui */}
+      {/* Exemplo de botão de Comprar agora */}
+      <Button onClick={() => addToCart({ title: 'Produto exemplo', price: 25.00 })}>
+        Comprar agora
+      </Button>
     </AppContainer>
   );
 }
 
-export default Sacola
+export default Sacola;
