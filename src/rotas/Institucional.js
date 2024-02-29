dc6f2ea993393a3855769bd9967ea4c4fed9b2bb
cleberfarias <cleber_afd@hotmail.com>
2024-02-29 18:06:13 -0300

    Fotos Galeria

--- src/rotas/Institucional.js
+++ src/rotas/Institucional.js
@@ -9,6 +9,7 @@ import Header from "../componentes/Header/Header";
 
 
 
+
 const AppContainer = styled.div`
   width: 100vw;
   min-height: 100vh;
@@ -45,7 +46,7 @@ const TextoInstitucional = styled.h3`
   padding: 30px 0;
   font-weight: 200;
   color: white;
-  font-size: 16px;
+  font-size: 25px;
   text-align: justify;
   margin: 0 auto;
   flex: 1;
@@ -65,9 +66,12 @@ const ImageContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
   gap: 20px;
-  justify-content: center;
-  align-items: center;
+  justify-content: space-between; /* Distribui as imagens pelo espaço disponível */
+  align-items: center; /* Alinha as imagens verticalmente */
   margin-top: 20px;
+  padding: 0 20px; /* Adiciona um espaçamento nas laterais */
+  max-width: 1200px; /* Define uma largura máxima para o container */
+  margin: 0 auto; /* Centraliza o container na página */
 `;
 
 const Image = styled.img`
@@ -75,75 +79,76 @@ const Image = styled.img`
   max-width: 320px;
   height: auto;
   object-fit: cover;
+  transition: transform 0.3s ease; /* Adiciona uma transição suave para a transformação */
+  cursor: pointer;
+
+  &:hover {
+    transform: scale(1.05); /* Aumenta ligeiramente o tamanho da imagem ao passar o mouse */
+  }
 `;
 
 function Institucional() {
   return (
     <div>
-      <Header/>
+      <Header />
       <AppContainer>
-      <header>
-        <ContentContainer>
-          <TextContainer>
-            <Titulo>Bem-vindo ao site da Cachaça<b> A Mais Indicada!</b></Titulo>
-            <TextoInstitucional>
-              <p>Nossa cachaça é produzida de maneira artesanal na encantadora cidade
-                de Celso Ramos, situada em Santa Catarina. Aqui, a tradição e a paixão pela
-                produção de cachaça estão enraizadas<br /> em nossa família, os Pelozato.
+        <header>
+          <ContentContainer>
+            <TextContainer>
+              <Titulo>Bem-vindo ao site da Cachaça<b> A Mais Indicada!</b></Titulo>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <Image className="imagem__producao" src={Producao1} alt="foto do alambique" />
+                <TextoInstitucional>
+                  <p>Nossa cachaça é produzida de maneira artesanal na encantadora cidade
+                    de Celso Ramos, situada em Santa Catarina. Aqui, a tradição e a paixão pela
+                    produção de cachaça estão enraizadas em nossa família, os Pelozato.</p>
+                </TextoInstitucional>
+              </div>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <TextoInstitucional>
+                  <p>Nossos fundadores são verdadeiros conhecedores da arte da destilação,
+                    utilizando técnicas tradicionais. Cada etapa do processo de produção é cuidadosamente
+                    supervisionada para garantir que nossa cachaça atinja os mais altos padrões de excelência.</p>
+                </TextoInstitucional>
+                <Image className="imagem__producao" src={Producao2} alt="foto do alambique" />
+              </div>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <Image className="imagem__producao" src={Producao3} alt="foto do alambique" />
+                <TextoInstitucional>
+                  <p>Além disso, a Cachaça A Mais Indicada é envelhecida em tonéis de madeira especial,
+                    permitindo que os sabores se desenvolvam e se aperfeiçoem ao longo do tempo. O resultado é uma
+                    bebida suave, equilibrada e cheia de personalidade, que agrada aos paladares mais exigentes.</p>
+                </TextoInstitucional>
+              </div>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <TextoInstitucional>
+                  <p>Em nosso site, você encontrará informações detalhadas sobre nossos produtos, desde os diferentes tipos de cachaça que oferecemos até sugestões de como
+                    apreciá-la da melhor maneira possível.</p>
+                </TextoInstitucional>
+                <Image className="imagem__producao" src={require("../imagens/EXP1.jpg")} alt="foto do alambique" />
+              </div>
+              <Titulo>Galeria<b> A Mais Indicada!</b></Titulo>
+              <ImageContainer>
+                <Image className="imagem__producao" src={require("../imagens/EXP3.jpg")} alt="foto do alambique" />
+                <Image className="imagem__producao" src={require("../imagens/EXP1.jpg")} alt="foto do alambique" />
+                <Image className="imagem__producao" src={require("../imagens/EXP2.jpg")} alt="foto do alambique" />
+                <Image className="imagem__producao" src={require("../imagens/EXP4.jpg")} alt="foto do alambique" />
+                <Image className="imagem__producao" src={require("../imagens/EXP7.jpg")} alt="foto do alambique" />
+                <Image className="imagem__producao" src={require("../imagens/EXP9.jpg")} alt="foto do alambique" />
+                <Image className="imagem__producao" src={require("../imagens/EXP10.jpg")} alt="foto do alambique" />
+                <Image className="imagem__producao" src={require("../imagens/EXP12.jpg")} alt="foto do alambique" />
 
-                Fundada por Roberson Pelozato e Iziario Pelozato, membros da família
-                Pelozato, a Cachaça A Mais Indicada representa o compromisso de criar<br />
-                uma bebida de alta qualidade, preservando a autenticidade e os métodos
-                artesanais que garantem seu sabor único.</p>
+              </ImageContainer>
+            </TextContainer>
 
-              <p>Nossos fundadores são verdadeiros conhecedores da arte da destilação,
-                utilizando técnicas tradicionais.
-                Cada etapa do processo de<br /> produção é cuidadosamente supervisionada para
-                garantir que nossa cachaça atinja os mais altos padrões de excelência.
-
-                O segredo de nossa cachaça reside em nossos ingredientes selecionados<br /> e na
-                atenção meticulosa aos detalhes durante o processo de produção. Utilizamos
-                apenas cana-de-açúcar de qualidade superior, cultivada localmente em nossas
-                terras, garantindo a riqueza<br /> de sabores e aromas em cada gole.</p>
-
-              <p>Além disso, a Cachaça A Mais Indicada é envelhecida em tonéis de madeira
-                especial, permitindo que os sabores se desenvolvam e se aperfeiçoem ao longo do tempo.
-                O resultado é uma<br /> bebida suave, equilibrada e cheia de personalidade, que agrada aos paladares
-                mais exigentes.
-
-                Nossa cachaça é o reflexo do amor que temos por nossa região e tradição.
-                Cada garrafa é produzida <br />com carinho e dedicação, e estamos orgulhosos de
-                compartilhar essa paixão com você.</p>
-
-              <p>Em nosso site, você encontrará informações detalhadas sobre nossos produtos,
-                desde os diferentes tipos de cachaça que oferecemos até sugestões de como
-                apreciá-la da melhor maneira<br /> possível. Também compartilharemos receitas
-                exclusivas de coquetéis que destacam as nuances e sabores de nossa
-                cachaça.</p>
-
-              <p>Além disso, temos o prazer de oferecer uma variedade de licores finos que
-                adicionam um sabor excepcional para qualquer ocasião. Elaborados com a mesma
-                paixão e qualidade que caracterizam nossa cachaça, nossos licores são a escolha
-                perfeita para aqueles que buscam uma experiência única.</p>
-            </TextoInstitucional>
-            <ImageContainer>
-              <Image className="imagem__producao" src={Producao1} alt="foto do alambique" />
-              <Image className="imagem__producao" src={Producao2} alt="foto do alambique" />
-              <Image className="imagem__producao" src={Producao3} alt="foto do alambique" />
-
-            </ImageContainer>
-            <TextoAgradecimento> Agradecemos por visitar o site da Cachaça A Mais Indicada e convidamos
-              você a experimentar a riqueza de nossa cachaça artesanal. Esteja preparado
-              para uma experiência única e inesquecível<br /> que celebra a tradição familiar e
-              a paixão pela cachaça.</TextoAgradecimento>
-          </TextContainer>
-        </ContentContainer>
-      </header>
+          </ContentContainer>
+        </header>
       </AppContainer>
-      <Footer/>
+      <Footer />
     </div>
-    
+
   );
 }
+
 
 export default Institucional;
